70dc5ab20062fe8b86d066ed96ad48d1a664009e
damiandominella <damiandominella@gmail.com>
2021-07-21 08:33:38 +0200

    js height

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+function resetHeight() {
+  // reset the body height to that of the inner browser
+  document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
