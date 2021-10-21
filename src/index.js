c35ee3d528e5e98bdcfe029266c4ab5f4a4cce1d
damiandominella <damiandominella@gmail.com>
2021-10-21 13:53:13 +0200

    call

--- src/index.js
+++ src/index.js
@@ -9,7 +9,23 @@ function resetHeight() {
   const element = document.getElementById("full-height");
   element.style.height = window.innerHeight + "px";
 }
+
+function resetLinks() {
+  var elements = document.getElementsByTagName("a");
+
+  for (let element of elements) {
+    element.onclick = function (event) {
+      event.preventDefault();
+
+      window.location.href =
+        element.getAttribute("href") + window.location.hash;
+    };
+  }
+}
+
 // reset the height whenever the window's resized
 // window.addEventListener("resize", resetHeight);
 // called to initially set the height.
 resetHeight();
+
+resetLinks();
